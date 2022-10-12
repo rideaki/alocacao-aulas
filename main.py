3f3672626e806df65a2cc6c552179fb46c7aa8c4
rideaki <reginaldoime@gmail.com>
2022-10-12 16:06:59 -0300

    Add utils para dias da semana. Constante Final

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from model.entities import Teacher
+from model.entity.teacher import Teacher
+from model.utils.daysOfWeek import FRIDAY, MONDAY, SATURDAY, THURSDAY, TUESDAY, WEDNESDAY
 
 def main():
     print ("Olá mundo!")
 
 if __name__ == '__main__':
-    teacherReginaldo = Teacher ("Reginaldo R.K.", [])
+    teacherReginaldo = Teacher ("Reginaldo R.K.", [MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY])
     print(teacherReginaldo.name)
+    print(teacherReginaldo.availabilities)
     input("Digite enter para finalizar.")
---
+++ model/utils/daysOfWeek.py
@@ -0,0 +1,11 @@
+from ast import Constant
+
+
+from typing import Final
+
+MONDAY: Final = 0
+TUESDAY: Final = 1
+WEDNESDAY: Final = 2
+THURSDAY: Final = 3
+FRIDAY: Final = 4
+SATURDAY: Final = 5
