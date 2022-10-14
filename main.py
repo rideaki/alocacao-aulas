afde386f83062823223457a2f01af744217b6ade
rideaki <reginaldoime@gmail.com>
2022-10-14 16:08:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from numpy import *
 import dataLoader
+import random
 from model.business import filter
 from dataLoader import loadBlocksOfCurricularComponentWithTeachers, loadClasses, loadTeachers
 from model.utils.shifts import AFTERNOON, MORNING
@@ -10,9 +10,17 @@ if __name__ == "__main__":
     loadBlocksOfCurricularComponentWithTeachers()
 
     blocksIndexesByClass = {}
+    # Para cada turma
     for dataClass in dataLoader.getClassesCopy():
         blocksIndexesByClass[dataClass] = filter.filterBlocksIndexesBySemesterAndShift(
             dataLoader.getBlocks(), dataClass.semesterNumber, dataClass.shift)
+        
+        #Para cada professor
+        teachers =  dataLoader.getTeachersCopy()
+        while (len(teachers) > 0):    
+            teacher = teachers.pop(random.choice(list(teachers)))
+            print(teacher.name)
+        
         blocksOfGilvan1Morning = filter.filterBlocksIndexesByTeacher(blocksIndexesByClass[dataClass], "Gilvan")
 
     # alocando aulas
