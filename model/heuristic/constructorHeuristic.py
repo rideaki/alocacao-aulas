edd8fe4aba3116e5af65fd9d0503bd9c8d97c701
rideaki <reginaldoime@gmail.com>
2022-11-04 10:18:59 -0300

    Evitando efeito colateral nas matrizes de construção heurística

--- model/heuristic/constructorHeuristic.py
+++ model/heuristic/constructorHeuristic.py
@@ -1,5 +1,6 @@
-import random
+import copy
 import dataLoader
+import random
 from model.business import filter
 from model.business.tableFactory import constructClassTable
 
@@ -12,17 +13,17 @@ def constructHeusristicSolution():
     while (len(classes) > 0):
         classData = random.choice(classes)
         classes.remove(classData) 
-        timeTables[classData] = constructClassTable(None).copy()
-        filteredBlocksIndexesByClass[classData] = filter.filterBlocksIndexesByClassData(
-            dataLoader.getBlocks(), classData).copy()
+        timeTables[classData] = copy.deepcopy(constructClassTable(None))
+        filteredBlocksIndexesByClass[classData] = copy.deepcopy(filter.filterBlocksIndexesByClassData(
+            dataLoader.getBlocks(), classData))
         
         #Para cada professor
         teachers =  dataLoader.getTeachersCopy()
         while (len(teachers) > 0):
             teacher = teachers.pop(random.choice(list(teachers)))
-            teacherBlocksToBeAllocated = filter.filterBlocksIndexesByTeacher(filteredBlocksIndexesByClass[classData], teacher.name).copy()
+            teacherBlocksToBeAllocated = copy.deepcopy(filter.filterBlocksIndexesByTeacher(filteredBlocksIndexesByClass[classData], teacher.name))
             while(len(teacherBlocksToBeAllocated) > 0):
-                availableTeacherBlocksByShift = teacher.getAllSortedBlocksCopy()[classData.shift].copy()
+                availableTeacherBlocksByShift = teacher.getAllSortedBlocksCopy()[classData.shift]
                 for teacherAvailableBlock in availableTeacherBlocksByShift:
                     if(timeTables[classData][teacherAvailableBlock[0]][teacherAvailableBlock[1]] == None):
                         #alocar aula
