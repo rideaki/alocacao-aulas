05cdd391534cd124ee820cf748ff7287692e8913
rideaki <reginaldoime@gmail.com>
2022-11-05 18:55:19 -0300

    Update metaHeuristic.py

--- model/heuristic/metaHeuristic.py
+++ model/heuristic/metaHeuristic.py
@@ -1,4 +1,5 @@
 import copy
+import dataLoader
 import numpy
 
 def searchMetaHeuristicSolution(initialSolutionArg, penaltiesTablesDict):
@@ -10,20 +11,50 @@ def searchMetaHeuristicSolution(initialSolutionArg, penaltiesTablesDict):
     neighborSolutions = __generateNeighborSolutions(initialSolutionArg, maxClassData, maxPenaltyIndexes)
 
 
+
 #Gerando permutações da timeTable[maxClassData] com indices maxPenaltyIndexes
-def __generateNeighborSolutions(initialSolutionArg, maxClassData, maxPenaltyIndexes):
+def __generateNeighborSolutions(initialSolution, maxClassData, maxPenaltyIndexes):
     neighborSolutions = []
-    timeTableWithMaxPenalty = copy.deepcopy(initialSolutionArg[maxClassData])
+    timeTableWithMaxPenalty = copy.deepcopy(initialSolution[maxClassData])
     maxBlockValue = timeTableWithMaxPenalty[maxPenaltyIndexes[0]][maxPenaltyIndexes[1]]
-    for i in range(len(timeTableWithMaxPenalty)): #percorre todas celulas da matriz
+    for i in range(len(timeTableWithMaxPenalty)): 
         for j in range(len(timeTableWithMaxPenalty[i])):
-            neighborSolution = initialSolutionArg.copy()
+            neighborSolution = initialSolution.copy()
             classNeighborSolution = copy.deepcopy(timeTableWithMaxPenalty)
             classNeighborSolution[maxPenaltyIndexes[0]][maxPenaltyIndexes[1]] = classNeighborSolution[i][j]
             classNeighborSolution[i][j] = maxBlockValue
-            neighborSolution[maxClassData] = copy.deepcopy(classNeighborSolution) 
-            neighborSolutions.append(neighborSolution)
+            __filterEqualSolution(maxClassData, neighborSolutions, timeTableWithMaxPenalty, neighborSolution, classNeighborSolution)
     return neighborSolutions
+
+def __filterEqualSolution(maxClassData, neighborSolutions, timeTableWithMaxPenalty, neighborSolution, classNeighborSolution):
+    if not __areEquivalentTables(classNeighborSolution, timeTableWithMaxPenalty):
+        neighborSolution[maxClassData] = copy.deepcopy(classNeighborSolution) 
+        neighborSolutions.append(neighborSolution)
+
+#Verifica se duas timeTables são iguais ou equivalentes, ou seja, cuja alocação de blocos são as mesmas
+def __areEquivalentTables(timeTable1, timeTable2):
+    if numpy.array_equal(timeTable1, timeTable2):  #se as duas tabelas são iguais
+        return True
+    for i in range(len(timeTable1)):
+        for j in range(len(timeTable1[i])):
+            if __areDifferentBlocks(timeTable1[i][j], timeTable2[i][j]):
+                return False
+    return True
+
+#Recebe indices de dois blocos, e retorna: 
+# False: se os indices são iguais, ou se são blocos do mesmo componente curricular e mesmo professor
+# True: caso contrário
+def __areDifferentBlocks(block1Index, block2Index):
+    if block1Index == block2Index:
+        return False
+    if (block1Index == None and block2Index != None) or (block1Index != None and block2Index == None):
+        return True
+    blocks = dataLoader.getBlocksCopy()
+    block1 = blocks[block1Index]
+    block2 = blocks[block2Index]
+    return (block1.curricularComponentName != block2.curricularComponentName) or (
+        block1.teacher.name != block2.teacher.name
+    )               
 
 def __searchMaxPenalty(penaltiesTablesDict):
     maxGlobalPenalty = 0
