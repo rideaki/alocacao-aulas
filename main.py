aaaea395c7bc17805941590a9b02daa4c1b9ddef
rideaki <reginaldoime@gmail.com>
2022-11-09 00:53:22 -0300

    Refactor

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import dataLoader
 
 from model.business.solutionAnalyzer import analyzeSolution, globalSolutionPenalty
+from model.constraints.teacherConstrainsts import SPARSE_DAYS_PENALTY
 from model.heuristic.constructorHeuristic import constructHeusristicSolution
 from model.heuristic.metaheuristic.tabuMetaHeuristic import searchTabuHeuristicSolution
-
-META_HEURISTIC_CYCLES = 180
 
 def searchHeuristicSolution():
     solution = constructHeusristicSolution().copy()
     penaltiesTablesDict = analyzeSolution(solution)
 
-    for i in range(META_HEURISTIC_CYCLES):
-        print("\n %d° Ciclo: " % i)
-        solution = searchTabuHeuristicSolution(solution.copy(), penaltiesTablesDict.copy())
-        penaltiesTablesDict = analyzeSolution(solution)
+    searchTabuHeuristicSolution(solution, penaltiesTablesDict)
 
 if __name__ == "__main__":
     dataLoader.loadAllData()
-    while(globalSolutionPenalty > 0):
+    while globalSolutionPenalty > 0:
         searchHeuristicSolution()
--- model/heuristic/metaheuristic/tabuMetaHeuristic.py
+++ model/heuristic/metaheuristic/tabuMetaHeuristic.py
@@ -3,14 +3,30 @@ import random
 import numpy
 
 from collections import deque
+from model.business.solutionAnalyzer import globalSolutionPenalty, globalSolution
 from model.business.comparator import areDifferentBlocks
-from model.constraints.teacherConstrainsts import calculatePenalties
+from model.business.solutionAnalyzer import analyzeSolution
+from model.constraints.teacherConstrainsts import SPARSE_DAYS_PENALTY, calculatePenalties
 
+META_HEURISTIC_CYCLES = 90
 TABU_SIZE = 40
 
 tabu = deque(maxlen=TABU_SIZE)
 
-def searchTabuHeuristicSolution(initialSolution, penaltiesTablesDict):
+def searchTabuHeuristicSolution(solution, globalPenaltiesTables):
+    __searchSolutionsInCycles(solution, globalPenaltiesTables)
+    if globalSolutionPenalty < SPARSE_DAYS_PENALTY:
+        globalPenaltiesTables = calculatePenalties(globalSolution)    
+        __searchSolutionsInCycles(globalSolution, globalPenaltiesTables)
+
+def __searchSolutionsInCycles(solution, penaltiesTablesDict):
+    tabu.clear()
+    for i in range(META_HEURISTIC_CYCLES):
+        print("\n %d° Ciclo: " % i)
+        solution = __searchBestNeighborSolution(solution.copy(), penaltiesTablesDict.copy())
+        penaltiesTablesDict = analyzeSolution(solution)
+
+def __searchBestNeighborSolution(initialSolution, penaltiesTablesDict):
     tabu.append(initialSolution)
     maxClassData = None
     maxPenaltyIndexes = None
