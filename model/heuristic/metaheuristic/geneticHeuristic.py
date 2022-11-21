5b47afcc5a5a1ad2dfeb3ac900a64fc38e987315
rideaki <reginaldoime@gmail.com>
2022-11-21 11:05:29 -0300

    Update geneticHeuristic.py

--- model/heuristic/metaheuristic/geneticHeuristic.py
+++ model/heuristic/metaheuristic/geneticHeuristic.py
@@ -2,25 +2,34 @@ import numpy
 
 from model.business.solutionAnalyzer import getGlobalSolution
 from model.constraints.teacherConstrainsts import calculatePenalties
+from model.constraints.teacherConstrainsts import SPARSE_DAYS_PENALTY
+from model.exporter.csvGenericExporter import exportToGenericCsvFile
 
 #Utiliza algoritmos geneticos na meta heuristica
 def searchGeneticHeuristicSolution(solution):
     father = getGlobalSolution().copy()
     mother = solution.copy()
     fatherPenalties, _ = calculatePenalties(father)
-    #O cromossomo é a classData cujo total de penalidades é a maior
-    chromosome = __searchMaxSumClassPenalty(fatherPenalties)
     son = father.copy()
-    son[chromosome] = mother[chromosome]
+    hasSparseDays = False
+    
+    exportToGenericCsvFile(father, "log.csv")
+    exportToGenericCsvFile(mother, "log.csv")
+    
+    #O cromossomo é a classData cujo timeTable será permutado
+    for chromosome, penaltyTable in fatherPenalties.copy().items():
+        localSumValue = numpy.sum(penaltyTable)
+        if localSumValue >= SPARSE_DAYS_PENALTY:
+            son[chromosome] = mother[chromosome]
+            hasSparseDays = True
+    
+    if not hasSparseDays:
+        for chromosome, penaltyTable in fatherPenalties.copy().items():
+            localSumValue = numpy.sum(penaltyTable)
+            if localSumValue > 0:
+                son[chromosome] = mother[chromosome]
+
+    exportToGenericCsvFile(son, "log.csv")
+
     return son
 
-def __searchMaxSumClassPenalty(penaltiesTablesDict):
-    maxSumPenalty = -1
-    # Para cada turma
-    for classData, penaltyTable in penaltiesTablesDict.copy().items():
-        localSumValue = numpy.sum(penaltyTable)
-        if localSumValue <= maxSumPenalty:
-            continue #maior penalidade da tabela não é o maximo de todas -> pula iteracao
-        maxSumPenalty = localSumValue
-        maxClassData = classData
-    return maxClassData
